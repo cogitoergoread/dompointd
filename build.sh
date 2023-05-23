52b22b6ea4c9760f9ba9acacddb216a7da8bd47c
Varga Herbarium Jozsef <31209755+cogitoergoread@users.noreply.github.com>
2023-05-23 10:42:32 +0200

    var 0.3, Python 3.11

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ python3 -m venv dist/venv
 source dist/venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r build_requirements.txt
-shiv --site-packages venv/lib/python3.10/site-packages \
+shiv --site-packages venv/lib/python3.11/site-packages \
 	--compressed \
 	-o dompointd-0.1 \
 	-e dompointd.__main__:main src/ \
