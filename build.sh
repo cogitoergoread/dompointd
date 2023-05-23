024bd0afb965966ada6bec06eb97727e65e819a3
Jozsef RH Varga <31209755+cogitoergoread@users.noreply.github.com>
2023-05-23 08:06:51 +0200

    Dompointd verzivvval

--- build.sh
+++ build.sh
@@ -2,11 +2,16 @@
 set -e
 set -x
 
+python3 -m venv  venv
+source dist/venv/bin/activate
+python3 -m pip install -r requirements.txt
+deactivate
+
 python3 -m venv dist/venv
 source dist/venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r build_requirements.txt
-shiv --dompointd venv/lib/python3.10/site-packages \
+shiv --site-packages venv/lib/python3.10/site-packages \
 	--compressed \
 	-o dompointd-0.1 \
 	-e dompointd.__main__:main src/ \
