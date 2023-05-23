d332873651928f6f75f4bdc38b2b9ec080a9f76f
Jozsef Varga Hupi <31209755+cogitoergoread@users.noreply.github.com>
2023-05-23 07:59:00 +0200

    Buid Edit

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ python3 -m venv dist/venv
 source dist/venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r build_requirements.txt
-shiv --site-packages /usrdata/local/pyenv/versions/fastapi/lib/python3.11/site-packages \
+shiv --dompointd venv/lib/python3.10/site-packages \
 	--compressed \
-	-o simple_api \
-	-e simple_api.__main__:main src/ \
+	-o dompointd-0.1 \
+	-e dompointd.__main__:main src/ \
 	--upgrade
